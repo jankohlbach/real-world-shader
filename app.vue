e120463e805919f0ad910a487dd2a52a4e37076b
Jan Kohlbach <mail@jankohlbach.dev>
2023-12-09 14:06:54 +0100

    ✨ create component for effect tile

--- app.vue
+++ app.vue
@@ -17,68 +17,50 @@ onMounted(() => {
     <section class="container">
       <h2>distortion effects</h2>
       <div class="grid">
-        <div class="tile">
-          <h3 class="h5">
-            effect 1
-          </h3>
-          <img src="/something.webp" data-webgl-media data-shader="distortion/001">
-          <a href="https://github.com/jankohlbach/real-world-shader/blob/main/assets/shader/distortion/001-vertex.glsl" target="_blank">vertex</a>
-          <a href="https://github.com/jankohlbach/real-world-shader/blob/main/assets/shader/distortion/001-fragment.glsl" target="_blank">fragment</a>
-        </div>
-        <div class="tile">
-          <h3 class="h5">
-            effect 2
-          </h3>
-          <img src="/something.webp" data-webgl-media data-shader="distortion/002">
-          <a href="https://github.com/jankohlbach/real-world-shader/blob/main/assets/shader/distortion/002-vertex.glsl" target="_blank">vertex</a>
-          <a href="https://github.com/jankohlbach/real-world-shader/blob/main/assets/shader/distortion/002-fragment.glsl" target="_blank">fragment</a>
-        </div>
+        <EffectTile
+          title="effect 1"
+          image-src="/something.webp"
+          shader="distortion/001"
+        />
+        <EffectTile
+          title="effect 2"
+          image-src="/something.webp"
+          shader="distortion/002"
+        />
       </div>
     </section>
     <section class="container">
       <h2>rgb effects</h2>
       <div class="grid">
-        <div class="tile">
-          <h3 class="h5">
-            effect 1
-          </h3>
-          <img src="/something.webp" data-webgl-media>
-        </div>
+        <EffectTile
+          title="effect 1"
+          image-src="/something.webp"
+        />
       </div>
     </section>
     <section class="container">
       <h2>background noise effects</h2>
       <div class="grid">
-        <div class="tile">
-          <h3 class="h5">
-            effect 1
-          </h3>
-          <img src="/something.webp" data-webgl-media>
-        </div>
-        <div class="tile">
-          <h3 class="h5">
-            effect 2
-          </h3>
-          <img src="/something.webp" data-webgl-media>
-        </div>
-        <div class="tile">
-          <h3 class="h5">
-            effect 3
-          </h3>
-          <img src="/something.webp" data-webgl-media>
-        </div>
-        <div class="tile">
-          <h3 class="h5">
-            effect 4
-          </h3>
-          <img src="/something.webp" data-webgl-media>
-        </div>
-        <div class="tile">
-          <h3 class="h5">
-            effect 5
-          </h3>
-          <img src="/something.webp" data-webgl-media>
-        </div>
+        <EffectTile
+          title="effect 1"
+          image-src="/something.webp"
+        />
+        <EffectTile
+          title="effect 2"
+          image-src="/something.webp"
+        />
+        <EffectTile
+          title="effect 3"
+          image-src="/something.webp"
+        />
+        <EffectTile
+          title="effect 4"
+          image-src="/something.webp"
+        />
+        <EffectTile
+          title="effect 5"
+          image-src="/something.webp"
+        />
       </div>
     </section>
   </main>
@@ -91,33 +73,5 @@ section {
 
 .grid {
   margin-top: clampFluid(20, 40);
-}
-
-.tile {
-  grid-column: span 12;
-}
-
-img {
-  margin-top: clampFluid(10, 20);
-}
-
-a {
-  display: block;
-  font-size: var(--fs-tiny);
-  font-weight: var(--fw-light);
-  line-height: 1.2;
-  text-decoration-thickness: 0.5px;
-}
-
-@media (min-width: 520px) {
-  .tile {
-    grid-column: span 6;
-  }
-}
-
-@media (min-width: 1024px) {
-  .tile {
-    grid-column: span 3;
-  }
 }
 </style>
---
+++ components/EffectTile.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  imageSrc: {
+    type: String,
+    required: true,
+    default: '/texture.jpg'
+  },
+  shader: {
+    type: String,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <div class="tile">
+    <h3 class="h5">
+      {{ title }}
+    </h3>
+    <img
+      :src="imageSrc"
+      :data-webgl-media="shader ? 'data-webgl-media' : undefined"
+      :data-shader="shader"
+    >
+    <a v-if="shader" :href="`https://github.com/jankohlbach/real-world-shader/blob/main/assets/shader/${shader}-vertex.glsl`" target="_blank">vertex</a>
+    <a v-if="shader" :href="`https://github.com/jankohlbach/real-world-shader/blob/main/assets/shader/${shader}-fragment.glsl`" target="_blank">fragment</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tile {
+  grid-column: span 12;
+}
+
+img {
+  margin-top: clampFluid(10, 20);
+}
+
+a {
+  display: block;
+  font-size: var(--fs-tiny);
+  font-weight: var(--fw-light);
+  line-height: 1.2;
+  text-decoration-thickness: 0.5px;
+}
+
+@media (min-width: 520px) {
+  .tile {
+    grid-column: span 6;
+  }
+}
+
+@media (min-width: 1024px) {
+  .tile {
+    grid-column: span 3;
+  }
+}
+</style>
